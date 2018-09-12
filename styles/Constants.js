45fa93ef7a89277ddfe94a2f7f9c4b85873325d7
Brandon Lew <brandon.t.lew@gmail.com>
2018-09-11 22:38:48 -0700

    add red

--- styles/Constants.js
+++ styles/Constants.js
@@ -1,5 +1,6 @@
 // STYLE VARIABLES
 export const colorAqua = '#6BB7C1';
 export const colorBlue = '#2E4E74';
-export const colorLightGrey = "#FDFDFD";
+export const colorLightGrey = '#FDFDFD';
 export const colorDarkGrey = '#494F56';
+export const colorRed = '#BF0000'
